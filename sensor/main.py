e4d095f3d9aa55c5f80542e666e2a40ef006f6d7
jtertil <jtertil@gmail.com>
2020-03-22 17:06:20 +0100

    refactor WIP

--- sensor/main.py
+++ sensor/main.py
@@ -1,77 +1,177 @@
 import time
 import urequests
-import uasyncio
-from setup import button, led, sensor, display, ap, wifi, WLAN_SSID, \
-    WLAN_PASSW, API_KEY, API_URL, SENSOR_ID, READ_INTERV
+from setup import button, led_config, sensor_config, display_config, \
+    connection_config, LOG_INTERVAL
 
 
-def display_data(temperature, humidity):
-    display.fill(0)
-    display.text('temp.: {} C'.format(temperature), 0, 0)
-    display.text('humidity: {} %'.format(humidity), 0, 32)
-    display.show()
+class ConnectionControl:
+    def __init__(self, connection_config):
+        self.ssid = connection_config['WIFI_SSID']
+        self.passw = connection_config['WIFI_PASSW']
+        self.ap = connection_config['AP_MODE']
+        self.wifi = connection_config['WIFI_MODE']
+        self.key = connection_config['API_KEY']
+        self.url = connection_config['API_URL']
+        self.sensor_id = connection_config['SENSOR_ID']
+        self.wifi_status = self.wifi.status()
+        self.api_status = 0
 
+    def get_wifi_status(self):
+        return self.wifi.status()
 
-def sensor_read():
-    sensor.measure()
-    led_blink(1)
-    return sensor.temperature(), sensor.humidity()
+    def get_api_status(self):
+        return self.api_status
 
+    def set_wifi_mode(self):
+        self.ap.active(False)
+        self.wifi.active(True)
 
-def connect_to_wifi(ssid=WLAN_SSID, passw=WLAN_PASSW):
-    ap.active(False)
-    wifi.active(True)
+    def set_ap_mode(self):
+        self.wifi.active(False)
+        self.ap.active(True)
 
-    while not wifi.isconnected():
-        led_blink(3)
-        wifi.connect(ssid, passw)
-        time.sleep(5)
+    def connect_to_wifi(self):
+        self.set_wifi_mode()
 
+        while not self.wifi.isconnected():
+            self.wifi.connect(self.ssid, self.passw)
+            time.sleep(5)
 
-def log_data(temperature, humidity):
-    data = {'sensor_id': SENSOR_ID,
-            'key': API_KEY,
-            'values': {
-                'temperature': temperature,
-                'humidity': humidity
+    def disconnect_wifi(self):
+        self.wifi.disconnect()
+
+    def send_to_api(self, t, h):
+        data = {'sensor_id': self.sensor_id,
+                'key': self.key,
+                'values': {
+                    'temperature': t,
+                    'humidity': h
+                    }
                 }
-            }
-
-    try:
-        urequests.post(API_URL, json=data)
-    except Exception as e:
-        print(e)
-
-
-def led_blink(num):
-    for i in range(num):
-        led.on()
-        time.sleep(0.2)
-        led.off()
-        time.sleep(0.2)
-    led.on()
+        try:
+            r = urequests.post(self.url, json=data)
+            self.api_status = r.status_code
+        except Exception as e:
+            self.api_status = 0
+            print(e)
 
 
-async def run():
-    while True:
-        if wifi.isconnected():
-            t, h = sensor_read()
-            display_data(t, h)
-            log_data(t, h)
-            await uasyncio.sleep(60 * READ_INTERV)
+class LedControl:
+    def __init__(self, led_config):
+        self.l = led_config
+
+    def set_on(self):
+        self.l.value(0)
+
+    def set_off(self):
+        self.l.value(1)
+
+    def toggle(self):
+        if self.l.value():
+            self.set_on()
         else:
-            connect_to_wifi()
+            self.set_off()
+
+    def blink(self, num=1, duration=0.5):
+        start_status = self.l.value()
+        self.set_off()
+        time.sleep(1)
+        for i in range(num):
+            self.set_on()
+            time.sleep(duration)
+            self.set_off()
+            time.sleep(duration)
+        time.sleep(1)
+        self.l.value(start_status)
+
+    def blink_err(self, num):
+        self.blink(10, 0.1)
+        time.sleep(1)
+        self.blink(num)
 
 
-async def display_toggle():
+class DisplayControl:
+    def __init__(self, display_config):
+        self.d = display_config
+        self.d.poweron()
+        self.is_active = True
+
+    def show_status(self, wifi_status, api_status):
+        self.d.fill(0)
+        self.d.text(
+            'wifi: {} api: {}'.format(wifi_status, api_status), 0, 48)
+        self.d.show()
+
+    def show_temp_humidity(self, t, h):
+        self.d.fill(0)
+        self.d.text('temp.: {} C'.format(t), 0, 0)
+        self.d.text('humidity: {} %'.format(h), 0, 16)
+        self.d.show()
+
+    def show_temp_humidity_status(self, t, h, wifi_status, api_status):
+        self.d.fill(0)
+        self.d.text('temp.: {} C'.format(t), 0, 0)
+        self.d.text('humidity: {} %'.format(h), 0, 16)
+        self.d.text(
+            'wifi: {} api: {}'.format(wifi_status, api_status), 0, 48)
+        self.d.show()
+
+    def set_on(self):
+        self.d.poweron()
+        self.is_active = True
+
+    def set_off(self):
+        self.d.poweroff()
+        self.is_active = False
+
+    def toggle(self):
+        if self.is_active:
+            self.set_off()
+        else:
+            self.set_on()
+
+
+class SensorControl:
+    def __init__(self, sensor_config):
+        self.s = sensor_config
+
+    def read(self):
+        self.s.measure()
+        return self.s.temperature(), self.s.humidity()
+
+
+con = ConnectionControl(connection_config)
+led = LedControl(led_config)
+display = DisplayControl(display_config)
+sensor = SensorControl(sensor_config)
+
+
+def run():
+    led.blink(5)
+    t, h = sensor.read()
+    display.show_temp_humidity_status(
+        t, h, con.get_wifi_status(), con.get_api_status())
+    i = 0
     while True:
-        if button.value() == 0:
-            display.poweron()
-            await uasyncio.sleep(5)
-            display.poweroff()
+        if i < 60 * LOG_INTERVAL:
+            if button.value() == 0:
+                display.toggle()
+                time.sleep(1)
+
+            i += 1
+            time.sleep(1)
+
+        else:
+            if con.wifi_status != 5:
+                led.blink_err(3)
+                con.connect_to_wifi()
+
+            led.blink()
+            t, h = sensor.read()
+            con.send_to_api(t, h)
+            display.show_temp_humidity_status(
+                t, h, con.get_wifi_status(), con.get_api_status())
+            i = 0
 
 
-loop = uasyncio.get_event_loop()
-loop.create_task(display_toggle())
-loop.create_task(run())
-loop.run_forever()
+run()
--- sensor/setup.py
+++ sensor/setup.py
@@ -15,32 +15,34 @@ I2C_SCL_PIN = pins['D5']
 I2C_SDA_PIN = pins['D6']
 
 # led config
-led = Pin(LED_PIN, Pin.OUT)
+led_config = Pin(LED_PIN, Pin.OUT)
 
 # sensor config
-sensor = DHT22(Pin(DHT22_PIN))
+LOG_INTERVAL = 5  # in minutes
+sensor_config = DHT22(Pin(DHT22_PIN))
+
 
 # display config
 i2c = I2C(
     scl=Pin(I2C_SCL_PIN),
     sda=Pin(I2C_SDA_PIN))
 i2c.scan()
-display = SSD1306_I2C(128, 64, i2c)
+display_config = SSD1306_I2C(128, 64, i2c)
 
 # button config
 BUTTON_PIN = pins['D1']
 button = Pin(BUTTON_PIN, Pin.IN, Pin.PULL_UP)
 
-# WLAN config
-WLAN_SSID = ''
-WLAN_PASSW = ''
+# connection config
+connection_config = {
+    'WIFI_SSID': '',
+    'WIFI_PASSW': '',
+    'AP_MODE': WLAN(AP_IF),  # access point mode
+    'WIFI_MODE': WLAN(STA_IF),  # wifi mode
+    'API_KEY': '',
+    'API_URL': '',
+    'SENSOR_ID': 1,
+}
 
-ap = WLAN(AP_IF)  # access point mode
-wifi = WLAN(STA_IF)  # wifi mode
 
 
-API_KEY = ''
-API_URL = ''
-SENSOR_ID = 1
-
-READ_INTERV = 5  # read interval in minutes
